6ee92270f6cd1d2e526482e3fc5e54dcb7fd1cd8
lldelisle <lucille.delisle@epfl.ch>
2017-09-01 14:53:09 -0400

    Add the possibility to have headers

--- tools/cooler/cooler_getMatrix.py
+++ tools/cooler/cooler_getMatrix.py
@@ -7,17 +7,36 @@ def checkInput(args):
   if args.r2 is None:
     args.r2=args.r1
 
+def getHeaders(c,r,a):
+  fragID=c.bins().fetch(r).axes[0]
+  chromID=c.bins().fetch(r).as_matrix(['chrom'])
+  starts=c.bins().fetch(r).as_matrix(['start'])
+  ends=c.bins().fetch(r).as_matrix(['end'])
+  return([ "%s|%s|%s:%i-%i"%(i,a,c[0],s,e) for i,c,s,e in zip(fragID,chromID,starts,ends) ])
+
 def extractMatrix(args):
   c = cooler.Cooler(args.input)
   mat=c.matrix(balance=args.balance).fetch(args.r1,args.r2)
-  with open(args.output, 'w') as f:
-    for row in mat:
-      line = '\t'.join("%.6g"% value for value in row)
+  if args.header:
+    ass=c.info['genome-assembly']
+    rowH=getHeaders(c,args.r1,ass)
+    colH=getHeaders(c,args.r2,ass)
+    with open(args.output, 'w') as f:
+      line = "%ix%i"%(len(rowH),len(colH)) + '\t' + '\t'.join(colH)
       f.write(line + '\n')
+      for i,row in enumerate(mat):
+        line = rowH[i]+'\t'+'\t'.join("%.6g"% value for value in row)
+        f.write(line + '\n')
+  else:
+    with open(args.output, 'w') as f:
+      for row in mat:
+        line = '\t'.join("%.6g"% value for value in row)
+        f.write(line + '\n')
 
 argp=argparse.ArgumentParser(description='Extract a matrix from a cool file.')
 argp.add_argument('--input',default=None, help='a cool file to extract from.')
 argp.add_argument('--output',default=None, help='a txt file with matrix values.')
+argp.add_argument('--header',action="store_true", help='If you want to print row names and col names (by default there are only values).')
 argp.add_argument('--balance',action="store_true", help='If you want the balanced values (by default it is the raw).')
 argp.add_argument('--r1',default=None,help='the region 1 on which you should generate the matrix')
 argp.add_argument('--r2',default=None,help='the region 2 if different from region1.')
